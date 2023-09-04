165d02974a9ad87e041a966be9294e272183a13a
Awayume <dev@awayume.jp>
2023-09-04 21:29:27 +0900

    add: Add feature flags `http` and `utils`

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 // SPDX-FileCopyrightText: 2023 Awayume <dev@awayume.jp>
 // SPDX-License-Identifier: Apache-2.0
 
+#[cfg(feature = "http")]
 pub mod http;
+
+#[cfg(feature = "utils")]
 pub mod utils;
