af1f011ba9d33951107d60d8fe3a94162bb1d0ed
Awayume <dev@awayume.jp>
2023-09-04 21:08:10 +0900

    change: Fix re-export of external libraries

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 // SPDX-FileCopyrightText: 2023 Awayume <dev@awayume.jp>
 // SPDX-License-Identifier: Apache-2.0
 
-pub use async_trait::async_trait;
-
-pub mod error;
 pub mod http;
+pub mod utils;
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,6 @@
 // SPDX-FileCopyrightText: 2023 Awayume <dev@awayume.jp>
 // SPDX-License-Identifier: Apache-2.0
 
-pub use crate::http::error::*;
+pub mod external {
+    pub use async_trait::async_trait;
+}
